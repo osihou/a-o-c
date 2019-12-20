6eb44704725aff487ed5e407c15bd8dcf7e1ace2
osihou <osihou@protonmail.com>
2019-12-20 20:17:22 +0000

    Day 2

--- 19/day2/day2.lisp
+++ 19/day2/day2.lisp
@@ -1,148 +1,92 @@
 ;;;Day 2
-
-;;***FIRST***
-
+(format t "Day 2~%")
+(defvar *filename* 'day2/input.md)
 
 
 (defun open-file (filename)
   (open filename :if-does-not-exist nil))
 
-(defun close-file (inx)
-  (close inx))
 
-
-(defun read-and-print (inx)
+(defun read-file (inx)
   (when inx
-    (loop for line = (read-line inx nil)
-		    while line
-		    collect line)))
+    (loop :for line = (read-line inx nil)
+	  :while line
+	  :collect line)))
 
-(defun decode-string (s)
- (labels ((f (lst) 
-	     (when lst
-	       (case (car lst)
-		 (#\, (cons #\space (f (cdr lst))))
-		 (otherwise  (cons (car lst) (f (cdr lst))))))))
-   (coerce (f (coerce s 'list)) 'string)))
-
-
-(defun decode-int (s)
-  (loop :for (integer position) := (multiple-value-list
-				     (parse-integer s
-						    :start (or position 0)
-						    :junk-allowed t))
-	:while integer
-	:collect integer))
-
-(defun parse-input (in)
-  (decode-int
-    (decode-string
-      (car (read-and-print in)))))
+(defun parse-string (s)
+  (if s
+    (if (position #\, s)
+      (cons (subseq s 0 (position #\, s))
+	    (parse-string (subseq s (+ 1 (position #\, s)))))
+      (cons s nil))))
 
 
-
-(defun add (program stack)
-  "Executes the add command"
-  (progn
-    (setf 
-      (nth (caddr stack) program) 
-      (+ 
-	(nth (car stack) program) 
-	(nth (cadr stack) program)))
-    (int-machine program (cdddr stack))))
-
-(defun mult (program stack)
-  "Executes the mult command"
-  (progn
-    (setf 
-      (nth (caddr stack) program) 
-      (* 
-	(nth (car stack) program) 
-	(nth (cadr stack) program)))
-    (int-machine program (cdddr stack))))
-
-(defun int-machine (program stack )
-  "recursive int machine"
-  (if (= (car stack) 99 )
-    nil
-  (case (car stack)
-    (1 ( add program (cdr stack)))
-    (2 ( mult program (cdr stack))))))
-
-(defun restore (program p n)
-  "restore computer to initial condition"
-  (setf (nth p program) n))
-
-(defvar in (open-file 'day2/input.md))
-
-(defvar *programe* (parse-input in))
-
-(defun noun-verb (n v)
-  (progn
-    (restore  *programe* 1 n)
-    (restore  *programe* 2 v)))
+(defun get-input ()
+  (mapcar #'parse-integer (parse-string (car (read-file (open-file *filename*))))))
 
 
-(noun-verb 12 2)
+(defun create-memory ()
+  (let* ((input (get-input))
+	 (ln (length input)))
+    (make-array ln :initial-contents input)))
 
-(print *programe*)
+(defun set-memory (arr p n)
+  (setf (aref arr p) n))
 
+(defun zero-mode (memory index)
+  (aref memory (aref memory index)))
 
-(int-machine *programe* *programe*)
-(print *programe*)
+(defun int-machine (memory)
+  (let ((index 0)
+	(opcode 0))
+    (loop 
+      (setf opcode (aref memory index))
+      (cond
+	((= opcode 1)
+	 (set-memory memory (aref memory (+ index 3)) (+ (zero-mode memory (+ index 1)) (zero-mode memory (+ index 2))))
+	 (incf index 4))
 
-(close-file in)
+	((= opcode 2)
+	 (set-memory memory (aref memory (+ index 3)) (* (zero-mode memory (+ index 1)) (zero-mode memory (+ index 2))))
+	 (incf index 4))
 
-(defun find-value (n v)
-  (progn
-    (setf *programe* (parse-input (open-file 'day2/input.md)))
-    (noun-verb n v)
-    (int-machine *programe* *programe*)
-    (car *programe*)))
-	
+	((= opcode 99) (return))))))
 
-(print (find-value 12 2))
-
-(defun active-search (var)
-  (loop	named outer 
-    	for n below 100
-	do
-	(loop for v below 100
-	      when ( = (find-value n v) var)
-	      do
-		(return-from outer (cons n v)))))
-
-
-(defvar *answer* (active-search 19690720))
-(print *answer*)
-(print (find-value (car *answer*)  (cdr *answer*)))
-
-
-(defun calc (n v)
-  (print (+ (* 100 n) v)))
-
-(calc (car *answer*) (cdr *answer*))
+(defun get-output (n v)
+  (let ((mem (create-memory)))
+    (progn
+      (set-memory mem 1 n)
+      (set-memory mem 2 v)
+      (int-machine mem)
+      (aref mem 0))))
 
 
-;;***TESTS***
-(print 'TESTS)
+(defun day2-first ()
+  (format t "First: ~d~%" (get-output 12 2)))
 
-(defvar *test1* '( 1 0 0 0 99))
-(int-machine *test1* *test1*)
-(print *test1*)
+(day2-first)
 
-(defvar *test2* '( 2 3 0 3 99))
-(int-machine *test2* *test2*)
-(print *test2*)
+(defvar *key* 19690720)
 
-(defvar *test3* '( 2 4 4 5 99 0))
-(int-machine *test3* *test3*)
-(print *test3*)
+(defun get-noun-verb (key)
+  (let* ((all (loop :for i
+		    :from 0
+		    :below 100
+		    :append
+		    (loop :for j
+			  :from 0
+			  :below 100
+			  :collect (cons i j))))
 
-(defvar *test4* '( 1 1 1 4 99 5 6 0 99))
-(int-machine *test4* *test4*)
-(print *test4*)
+	 (lst (loop :for i
+		    :in all
+		    :collect (get-output (first i) (rest i)))))
+
+    (nth  (- (length (member key (reverse lst))) 1) all)))
 
 
+(defun day2-second ()
+  (let ((answer (get-noun-verb *key*)))
+  (format t "Second: ~d~%" (+ (* 100 (first answer)) (rest answer)))))
 
-
+(day2-second)
