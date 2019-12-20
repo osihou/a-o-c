86efedc5c66add31ee2713adfe94f48e320903b9
osihou <osihou@protonmail.com>
2019-12-20 20:16:50 +0000

    Day 1

--- 19/day1/day1.lisp
+++ 19/day1/day1.lisp
@@ -1,46 +1,45 @@
 ;;;Day 1
+(format t "Day 1~%")
+(defvar *filename* 'day1/input.md )
 
-;;;***FIRST***
+(defun open-file (filename)
+  (open filename :if-does-not-exist nil))
+
+(defun read-file (inx)
+  (when inx
+    (loop :for line = (read-line inx nil)
+	  :while line
+	  :collect line)))
+
+(defun get-input ()
+  (mapcar #'parse-integer
+	  (read-file (open-file *filename*))))
 
 (defun sum-of-fuel (lst)
-  "Calculate the needed fuel for module"
   (reduce #'+
 	  (mapcar
 	    (lambda (x)
 	      (- (floor (/ x 3.0)) 2))
 	    lst)))
-;;tests
-(print 'TEST)
-(print (sum-of-fuel '(12 14 1969 100756)))
 
-(print '***SECOND***)
-;;;***SECOND***
+(defun day1-first ()
+  (format t "First: ~d~%" (sum-of-fuel (get-input))))
+
+
+(day1-first)
+
 (defun correction (value)
   (- (sum-of-fuels value) value))
 
 (defun sum-of-fuels (value)
-  "Recursive function that calculates the needed fuels"
    (let ((fuel 0))
     (if (< value 0)
       0
       (setf fuel 
 	    (+ fuel (+ value (sum-of-fuels  (sum-of-fuel (list value)))))))))
 
-(defun read-and-print (filename)
-  (let ((in (open filename :if-does-not-exist nil)))
-    (when in
-      (print 
-	(reduce #'+
-	(mapcar #'correction
-	(mapcar #'parse-integer 
-		(loop for line = (read-line in nil)
-		      while line  
-		      collect line)))))
-      (close in))))
+(defun day1-second ()
+  (format t "Second: ~d~%" (reduce #'+ (mapcar #'correction (get-input)))))
 
-(read-and-print "day1/input.md")
+(day1-second)
 
-;;tests
-(print 'TEST)
-(print (correction 14))
-(print (correction 100756))
